ce241aa3d2bb73be7256a160ebc740a74caf4541
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-20 18:48:50 +0200

    update

--- store-backend/migrations/sqls/20230801143335-orders-table-up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    status VARCHAR(50) NOT NULL
-);
--- store-backend/migrations/sqls/20230801143529-orders-products-table-up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE order_products (
-    id SERIAL PRIMARY KEY,
-    quantity INTEGER NOT NULL,
-    order_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL
-);
