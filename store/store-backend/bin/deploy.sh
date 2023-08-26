5a394b3587f878fabf2a0057947576e0ae709c5f
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-26 14:44:57 +0200

    u

--- store/store-backend/bin/deploy.sh
+++ store/store-backend/bin/deploy.sh
@@ -1,18 +1,4 @@
-
-# # Navigiere ins Backend-Verzeichnis
-# cd store/store-backend
-
-# # Installiere Abhängigkeiten und baue das Backend
-# npm install
-# npm run build
-
-# # Setze den Namen deiner Elastic Beanstalk-Umgebung
-# EB_ENV_NAME=Testumgebung-env
-
-# # Deploy zum Elastic Beanstalk
-# eb deploy $EB_ENV_NAME
-
-# #!/bin/bash
+#!/bin/bash
 
 
 # Initialisieren Sie die Elastic Beanstalk-Umgebung (nur beim ersten Mal notwendig oder wenn Sie die Umgebung ändern)
