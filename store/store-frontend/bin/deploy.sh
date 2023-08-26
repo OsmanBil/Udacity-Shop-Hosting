f9a7f6ce0d43f97b62504a1956120e85822ce4a2
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-26 12:06:01 +0200

    update

--- store/store-frontend/bin/deploy.sh
+++ store/store-frontend/bin/deploy.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Setzen Sie den Namen Ihres S3-Buckets
-BUCKET_NAME=udacity-bucket-00001
+# # Setzen Sie den Namen Ihres S3-Buckets
+# BUCKET_NAME=udacity-bucket-00001
 
-# Navigieren Sie zum Build-Verzeichnis
-cd ../dist
+# # Navigieren Sie zum Build-Verzeichnis
+# cd ../dist
 
-# Hochladen der Dateien auf S3
-aws s3 sync . s3://$BUCKET_NAME/ --acl public-read
+# # Hochladen der Dateien auf S3
+# aws s3 sync . s3://$BUCKET_NAME/ --acl public-read
+
+aws s3 cp --recursive --acl public-read ./dist s3://udacity-bucket-00001
