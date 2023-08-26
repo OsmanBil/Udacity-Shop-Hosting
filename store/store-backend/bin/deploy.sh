cc006b4dc12c09ebcb698b09b39ac6a9e2c01704
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-26 12:59:53 +0200

    u

--- store/store-backend/bin/deploy.sh
+++ store/store-backend/bin/deploy.sh
@@ -1,14 +1,28 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Navigiere ins Backend-Verzeichnis
+# # Navigiere ins Backend-Verzeichnis
+# cd store/store-backend
+
+# # Installiere Abhängigkeiten und baue das Backend
+# npm install
+# npm run build
+
+# # Setze den Namen deiner Elastic Beanstalk-Umgebung
+# EB_ENV_NAME=Testumgebung-env
+
+# # Deploy zum Elastic Beanstalk
+# eb deploy $EB_ENV_NAME
+
+
+# Navigieren Sie zum Backend-Verzeichnis
 cd store/store-backend
 
-# Installiere Abhängigkeiten und baue das Backend
-npm install
-npm run build
+# Initialisieren Sie die Elastic Beanstalk-Umgebung (nur beim ersten Mal notwendig oder wenn Sie die Umgebung ändern)
+eb init -p [Node.js] [Testumgebung-env]
 
-# Setze den Namen deiner Elastic Beanstalk-Umgebung
-EB_ENV_NAME=Testumgebung-env
+# Erstellen Sie ein neues Anwendungs-Release und bereitstellen Sie es
+eb deploy
 
-# Deploy zum Elastic Beanstalk
-eb deploy $EB_ENV_NAME
+# Alternativ: Wenn Sie ein ZIP-Archiv für die Bereitstellung erstellen möchten
+# zip -r ../my-app.zip *
+# eb deploy --staged
