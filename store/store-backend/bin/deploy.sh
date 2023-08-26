d122db37261cd446e1f6e537bb65b67385b1533a
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-26 19:23:43 +0200

    u

--- store/store-backend/bin/deploy.sh
+++ store/store-backend/bin/deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 
+# Navigiere zum dist/src Verzeichnis
+cd dist/src
+
+# Erstelle ein ZIP-Archiv von allen Dateien in diesem Verzeichnis
+zip -r ../../my-app.zip *
+
 # Initialisieren Sie die Elastic Beanstalk-Umgebung (nur beim ersten Mal notwendig oder wenn Sie die Umgebung ändern)
 eb init "testumgebung" -p "Node.js 18" -r us-east-1
 
 # Erstellen Sie ein neues Anwendungs-Release und bereitstellen Sie es
 
-eb deploy "Testumgebung-env"
+eb deploy "Testumgebung-env" --staged
 
 # Alternativ: Wenn Sie ein ZIP-Archiv für die Bereitstellung erstellen möchten
 # zip -r ../my-app.zip *
