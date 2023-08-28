c8f00f0af30a069640f0ab4cd22665a5acb3743f
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-08-28 10:11:11 +0200

    up

--- store/store-backend/bin/deploy.sh
+++ store/store-backend/bin/deploy.sh
@@ -3,5 +3,7 @@
 # Initialisieren der Elastic Beanstalk-Umgebung (nur beim ersten Mal notwendig oder wenn Sie die Umgebung ändern)
 eb init "testumgebung" -p "Node.js 18" -r us-east-1
 
+eb setenv POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USER=$POSTGRES_USER
+
 # Erstellen Sie ein neues Anwendungs-Release und bereitstellen Sie es
 eb deploy "Testumgebung-env"
